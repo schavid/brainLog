b6d86ffcaa5e0f572abb4d76a8e5f18a6462d824
David <david.scheu97@gmail.com>
2025-04-17 20:11:39 +0200

    added search button and animations for searchbar

--- app/src/main/java/com/example/brainlog/MainActivity.kt
+++ app/src/main/java/com/example/brainlog/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.brainlog.ui.theme.BrainLogTheme
 import com.example.brainlog.view.Home
+import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+
+
 
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BrainLogTheme {
-                Scaffold(
-                    // Falls du später z.B. topBar oder bottomBar verwenden möchtest, kannst du hier die entsprechenden Komponenten einfügen.
-                    content = { innerPadding ->
-                        // Hier wird der Homescreen gerendert. Wir übergeben das Padding aus dem Scaffold.
-                        Home(modifier = Modifier.padding(innerPadding))
-                    }
-                )
+                Home()
             }
         }
     }
