8918dddc58dea1722f68719bbd9a51075ce5998b
David <david.scheu97@gmail.com>
2025-05-15 22:49:54 +0200

    added minor changes

--- app/src/main/java/com/example/brainlog/view/ProfileScreen.kt
+++ app/src/main/java/com/example/brainlog/view/ProfileScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
@@ -18,9 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
@@ -113,8 +109,8 @@ fun ProfileScreen(
                         } else {
                             FlowRow (
                                 modifier = Modifier.fillMaxSize(),
-                                mainAxisSpacing = 8.dp, // Abstand zwischen den Elementen
-                                crossAxisSpacing = 8.dp, // Abstand zwischen den Zeilen
+                                horizontalArrangement = Arrangement.spacedBy(8.dp), // Abstand zwischen den Elementen
+                                verticalArrangement = Arrangement.spacedBy(8.dp), // Abstand zwischen den Zeilen
 
                             // Damit die LazyColumn die Breite füllt
                             ) {
